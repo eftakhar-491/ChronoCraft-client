4a2d7a630d737418e5bae9cd5052805578bbe0c6
eftakhar-491 <eftt042@gmail.com>
2024-12-22 01:53:26 +0600

    Featured Artifacts section design done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Featured from "../components/Featured";
 
 export default function Home() {
   return (
     <>
       <div className="max-w-[1600px] mx-auto ">
         <Header />
+        <Featured />
       </div>
     </>
   );
